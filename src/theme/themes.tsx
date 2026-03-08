29e88f668bb71006a3105b500fa97bf0f241e3f2
dressaDevv <dr3sa.dev@gmail.com>
2026-03-08 19:24:07 -0300

    fiz o front da pagina de login, e adicionei os temas

---
+++ src/theme/themes.tsx
@@ -0,0 +1,10 @@
+export const themas ={
+    colors: {
+        bgScreen: '#000000',
+        primary: '#5BBF26',
+        secundary: '#f7f7f7',
+        bgInputs: '#1D1D1D',
+        gray: '#0F0F0F',
+        platinium: '#EFEFEF'
+    }
+}
